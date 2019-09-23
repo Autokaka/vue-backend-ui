d73fa19349cbde67a7bf6ab21b24f35bcd46714b
Autokaka <785321595@qq.com>
2019-09-23 18:51:47 +0800

    Update footer component
    * First commit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,48 @@
 <template>
-  <div>Footer</div>
+  <el-row class="footer" :style="footer">
+    <el-col :style="license" :span="12">MIT Licensed | Copyright © 2002 - 2019 Njtech Green Studio.</el-col>
+    <el-col :style="struct" :span="12">Powered by Vue.js + ElementUI</el-col>
+  </el-row>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Footer",
+  props: {
+    theme: {
+      type: String,
+      default: "#2196F3"
+    },
+    text_color: {
+      type: String,
+      default: "white"
+    }
+  },
+  computed: {
+    footer() {
+      return {
+        background: this.theme
+      };
+    },
+    license() {
+      return {
+        "text-align": "center",
+        color: this.text_color
+      };
+    },
+    struct() {
+      return {
+        "text-align": "center",
+        color: this.text_color
+      };
+    }
+  }
+};
 </script>
 
 <style scoped>
+.footer {
+  height: 2rem;
+  line-height: 2rem;
+}
 </style>
